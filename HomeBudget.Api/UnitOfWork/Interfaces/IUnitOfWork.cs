11f168269263a9323ecd7a552f58cada3276a211
Tomasz Janiak <t.janiak@vrr.com.pl>
2024-10-09 16:59:26 +0200

    WIP: Implementacja wzorca Repository i UnitOfWork. Opakowanie repozytoriów jednostką pracy.

---
+++ HomeBudget.Api/UnitOfWork/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using HomeBudget.Api.Repositories.Interfaces;
+
+namespace HomeBudget.Api.UnitOfWork.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IUserRepository Users { get; }
+        IAddressRepository Addresses { get; }
+        IBudgetRepository Budgets { get; }
+        IExpenseCategoryRepository ExpenseCategories { get; }
+        IExpenseRepository Expenses { get; }
+
+        Task<int> SaveChangesAsync();
+    }
+}
