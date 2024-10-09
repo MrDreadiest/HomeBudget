20f9447e888d9f7f47b346811d8b1cd57110c70e
Tomasz Janiak <t.janiak@vrr.com.pl>
2024-10-09 16:55:01 +0200

    Dodanie podstawowych encji i relacji pomiędzy nimi.

--- HomeBudget.Api/Entities/User.cs
+++ HomeBudget.Api/Entities/User.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using HomeBudget.Common.EntityDTOs.User;
+using Microsoft.AspNetCore.Identity;
+using System.Text.Json.Serialization;
 
 namespace HomeBudget.Api.Entities
 {
@@ -6,5 +8,20 @@ namespace HomeBudget.Api.Entities
     {
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
+        public bool IsAccountConfirmed { get; set; } = false;
+        public bool IsAccountSetup { get; set; } = false;
+
+        #region AddressRelation
+        public Address? Address { get; set; } = null!;
+        #endregion
+
+        #region Budget
+        public List<Budget> Budgets { get; private set; } = new List<Budget>();
+        #endregion
+
+        public User() : base() 
+        {
+            Address = new Address(this);
+        }
     }
 }
