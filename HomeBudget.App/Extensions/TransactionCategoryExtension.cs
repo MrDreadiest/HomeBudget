fcd1c3d1d00b63eca160cad7f1f9286dd67db4cc
Tomasz Janiak <t.janiak@vrr.com.pl>
2024-11-07 13:39:28 +0100

    Dodanie rozszerzeń modeli podstawowych.

---
+++ HomeBudget.App/Extensions/TransactionCategoryExtension.cs
@@ -0,0 +1,117 @@
+﻿using HomeBudget.App.Models;
+using HomeBudget.Common.EntityDTOs.TransactionCategory;
+
+namespace HomeBudget.App.Extensions
+{
+    public static class TransactionCategoryExtension
+    {
+        public static void FromGetResponse(this TransactionCategory transactionCategory, TransactionCategoryGetResponseModel responseModel)
+        {
+            transactionCategory.Id = responseModel.Id;
+            transactionCategory.BudgetId = responseModel.BudgetId;
+            transactionCategory.IconUnicode = responseModel.IconUnicode;
+            transactionCategory.Name = responseModel.Name;
+        }
+
+        public static TransactionCategory FromGetResponse(this TransactionCategoryGetResponseModel responseModel)
+        {
+            return new TransactionCategory()
+            {
+                Id = responseModel.Id,
+                BudgetId = responseModel.BudgetId,
+                IconUnicode = responseModel.IconUnicode,
+                Name = responseModel.Name,
+            };
+        }
+
+        public static TransactionCategoryCreateRequestModel ToCreateRequest(this TransactionCategory transactionCategory)
+        {
+            return new TransactionCategoryCreateRequestModel()
+            {
+                Name = transactionCategory.Name,
+                IconUnicode = transactionCategory.IconUnicode,
+            };
+        }
+
+        public static TransactionCategory FromCreateResponse(this TransactionCategoryCreateResponseModel responseModel)
+        {
+            return new TransactionCategory()
+            {
+                Id = responseModel.Id,
+                BudgetId = responseModel.BudgetId,
+                Name = responseModel.Name,
+                IconUnicode = responseModel.IconUnicode,
+            };
+        }
+
+        public static void FromCreateResponse(this TransactionCategory transactionCategory, TransactionCategoryCreateResponseModel responseModel)
+        {
+            transactionCategory.Id = responseModel.Id;
+            transactionCategory.BudgetId = responseModel.BudgetId;
+            transactionCategory.Name = responseModel.Name;
+            transactionCategory.IconUnicode = responseModel.IconUnicode;
+        }
+
+        public static void FromUpdateResponse(this TransactionCategory transactionCategory, TransactionCategoryUpdateResponseModel responseModel)
+        {
+            transactionCategory.Id = responseModel.Id;
+            transactionCategory.BudgetId = responseModel.BudgetId;
+            transactionCategory.Name = responseModel.Name;
+            transactionCategory.IconUnicode = responseModel.IconUnicode;
+        }
+
+        public static TransactionCategoryUpdateRequestModel ToUpdateRequest(this TransactionCategory transactionCategory)
+        {
+            return new TransactionCategoryUpdateRequestModel()
+            {
+                Name = transactionCategory.Name,
+                IconUnicode = transactionCategory.IconUnicode,
+            };
+        }
+
+        public static bool IsRequestValid(this TransactionCategoryCreateRequestModel requestModel)
+        {
+            if (string.IsNullOrEmpty(requestModel.Name))
+                return false;
+
+            if (string.IsNullOrEmpty(requestModel.IconUnicode))
+                return false;
+
+            return true;
+        }
+
+        public static bool IsRequestValid(this TransactionCategoryUpdateRequestModel requestModel)
+        {
+            if (string.IsNullOrEmpty(requestModel.Name))
+                return false;
+
+            if (string.IsNullOrEmpty(requestModel.IconUnicode))
+                return false;
+
+            return true;
+        }
+
+
+        public static bool IsNullOrEmpty(this TransactionCategory transactionCategory)
+        {
+            if (transactionCategory == null)
+                return true;
+
+            if (string.IsNullOrEmpty(transactionCategory.Id))
+                return true;
+
+            if (string.IsNullOrEmpty(transactionCategory.BudgetId))
+                return true;
+
+            if (string.IsNullOrEmpty(transactionCategory.Name))
+                return true;
+
+            if (string.IsNullOrEmpty(transactionCategory.IconUnicode))
+                return true;
+
+            return false;
+        }
+
+
+    }
+}
---
+++ HomeBudget.App/Extensions/TransactionExtension.cs
@@ -0,0 +1,138 @@
+﻿using HomeBudget.App.Models;
+using HomeBudget.Common.EntityDTOs.Transaction;
+
+namespace HomeBudget.App.Extensions
+{
+    public static class TransactionExtension
+    {
+        public static void FromGetResponse(this Transaction transaction, TransactionGetResponseModel responseModel)
+        {
+            transaction.Id = responseModel.Id;
+            transaction.BudgetId = responseModel.BudgetId;
+            transaction.TransactionCategoryId = responseModel.TransactionCategoryId;
+            transaction.CreatorId = responseModel.CreatorId;
+            transaction.Name = responseModel.Name;
+            transaction.Description = responseModel.Description;
+            transaction.TotalAmount = responseModel.TotalAmount;
+            transaction.Date = responseModel.Date;
+        }
+
+        public static Transaction FromGetResponse(this TransactionGetResponseModel responseModel)
+        {
+            return new Transaction()
+            {
+                Id = responseModel.Id,
+                TransactionCategoryId = responseModel.TransactionCategoryId,
+                CreatorId = responseModel.CreatorId,
+                BudgetId = responseModel.BudgetId,
+                Name = responseModel.Name,
+                Description = responseModel.Description,
+                TotalAmount = responseModel.TotalAmount,
+                Date = responseModel.Date,
+            };
+        }
+
+        public static TransactionCreateRequestModel ToCreateRequest(this Transaction transaction)
+        {
+            return new TransactionCreateRequestModel()
+            {
+                TransactionCategoryId = transaction.TransactionCategoryId,
+                CreatorId = transaction.CreatorId,
+                BudgetId = transaction.BudgetId,
+                Name = transaction.Name,
+                Description = transaction.Description,
+                TotalAmount = transaction.TotalAmount,
+                Date = transaction.Date,
+            };
+        }
+
+        public static Transaction FromCreateResponse(this TransactionCreateResponseModel responseModel)
+        {
+            return new Transaction()
+            {
+                Id = responseModel.Id,
+                TransactionCategoryId = responseModel.TransactionCategoryId,
+                CreatorId = responseModel.CreatorId,
+                BudgetId = responseModel.BudgetId,
+                Name = responseModel.Name,
+                Description = responseModel.Description,
+                TotalAmount = responseModel.TotalAmount,
+                Date = responseModel.Date,
+            };
+        }
+
+        public static void FromUpdateResponse(this Transaction transaction, TransactionUpdateResponseModel responseModel)
+        {
+            transaction.Id = responseModel.Id;
+            transaction.BudgetId = responseModel.BudgetId;
+            transaction.TransactionCategoryId = responseModel.TransactionCategoryId;
+            transaction.CreatorId = responseModel.CreatorId;
+            transaction.Name = responseModel.Name;
+            transaction.Description = responseModel.Description;
+            transaction.TotalAmount = responseModel.TotalAmount;
+            transaction.Date = responseModel.Date;
+        }
+
+        public static TransactionUpdateRequestModel ToUpdateRequest(this Transaction transaction)
+        {
+            return new TransactionUpdateRequestModel()
+            {
+                TransactionCategoryId = transaction.TransactionCategoryId,
+                CreatorId = transaction.CreatorId,
+                BudgetId = transaction.BudgetId,
+                Name = transaction.Name,
+                Description = transaction.Description,
+                TotalAmount = transaction.TotalAmount,
+                Date = transaction.Date,
+            };
+        }
+
+        public static ValidationResult IsRequestValid(this TransactionCreateRequestModel requestModel)
+        {
+            if (string.IsNullOrEmpty(requestModel.BudgetId))
+                return new ValidationResult(false, "BudgetId cannot be null or empty.");
+
+            if (string.IsNullOrEmpty(requestModel.CreatorId))
+                return new ValidationResult(false, "CreatorId cannot be null or empty.");
+
+            if (string.IsNullOrEmpty(requestModel.TransactionCategoryId))
+                return new ValidationResult(false, "TransactionCategoryId cannot be null or empty.");
+
+            if (string.IsNullOrEmpty(requestModel.Name))
+                return new ValidationResult(false, "Name cannot be null or empty.");
+
+            if (requestModel.TotalAmount == 0)
+                return new ValidationResult(false, "TotalAmount cannot be zero.");
+
+            return new ValidationResult(true, string.Empty);
+        }
+
+        //public static bool IsRequestValid(this TransactionUpdateRequestModel requestModel)
+        //{
+
+        //}
+
+        public static bool IsNullOrEmpty(this Transaction transaction)
+        {
+            if (transaction == null)
+                return true;
+
+            if (string.IsNullOrEmpty(transaction.Id))
+                return true;
+
+            if (string.IsNullOrEmpty(transaction.BudgetId))
+                return true;
+
+            if (string.IsNullOrEmpty(transaction.CreatorId))
+                return true;
+
+            if (string.IsNullOrEmpty(transaction.TransactionCategoryId))
+                return true;
+
+            if (string.IsNullOrEmpty(transaction.Name))
+                return true;
+
+            return false;
+        }
+    }
+}
