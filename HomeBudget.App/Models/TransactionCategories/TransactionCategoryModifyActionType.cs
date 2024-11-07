6f95b4640c3af890ccf721e9f6e2870aecc527fc
Tomasz Janiak <t.janiak@vrr.com.pl>
2024-11-07 13:38:35 +0100

    Dodanie modeli pomocniczych.

---
+++ HomeBudget.App/Models/TransactionCategories/TransactionCategoryModifyActionType.cs
@@ -0,0 +1,24 @@
+﻿namespace HomeBudget.App.Models.TransactionCategories
+{
+    public enum TransactionCategoryModifyActionType
+    {
+        Create,
+        Update,
+    }
+
+    public static class TransactionCategoryModifyActionTypeExtensions
+    {
+        public static string GetDescription(this TransactionCategoryModifyActionType actionType)
+        {
+            switch (actionType)
+            {
+                case TransactionCategoryModifyActionType.Create:
+                    return Resources.Languages.AppResource.TransactionModifyActionType_Create;
+                case TransactionCategoryModifyActionType.Update:
+                    return Resources.Languages.AppResource.TransactionModifyActionType_Update;
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+}
---
+++ HomeBudget.App/Models/Transactions/TransactionFilterType.cs
@@ -0,0 +1,27 @@
+﻿namespace HomeBudget.App.Models.Transactions
+{
+    public enum TransactionFilterType
+    {
+        Expense = 0,
+        Income = 2,
+        All = 4,
+    }
+
+    public static class TransactionFilterTypeExtensions
+    {
+        public static string GetDescription(this TransactionFilterType transactionType)
+        {
+            switch (transactionType)
+            {
+                case TransactionFilterType.Expense:
+                    return Resources.Languages.AppResource.TransactionFilterType_Expense;
+                case TransactionFilterType.Income:
+                    return Resources.Languages.AppResource.TransactionFilterType_Income;
+                case TransactionFilterType.All:
+                    return Resources.Languages.AppResource.TransactionFilterType_All;
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+}
