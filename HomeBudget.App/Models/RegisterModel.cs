7e9dd253a0b8a5acfb0caaf2b63e6b1c6690a358
Tomasz Janiak <t.janiak@vrr.com.pl>
2024-11-07 13:38:04 +0100

    Wystawienie i poprawa modeli głównych.

--- HomeBudget.App/Models/RegisterModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HomeBudget.App.Models
-{
-    public class RegisterModel
-    {
-        public string? Email { get; set; }
-        public string? Password { get; set; }
-    }
-}
---
+++ HomeBudget.App/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace HomeBudget.App.Models
+{
+    public class User
+    {
+        public required string Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public bool IsAccountConfirmed { get; set; } = false;
+        public bool IsAccountSetup { get; set; } = false;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
